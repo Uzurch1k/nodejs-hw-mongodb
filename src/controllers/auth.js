f5f6c2beabcffd5f8ed736c202b38b3d2a1a4ebd
Uzurch1k <galcenkovlad7@gmail.com>
2024-06-29 21:36:34 +0300

    fix

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -78,8 +78,8 @@ export const refreshUserSessionController = async (req, res) => {
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
     status: 200,
+    message: 'Reset password email has been successfully sent.',
     data: {},
   });
 };
@@ -87,8 +87,8 @@ export const requestResetEmailController = async (req, res) => {
 export const resetPasswordController = async (req, res) => {
   await resetPassword(req.body);
   res.json({
-    message: 'Password was successfully reset!',
     status: 200,
+    message: 'Password has been successfully reset.',
     data: {},
   });
 };
